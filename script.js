5e32811f13ae5c886dd05ff73f9e6797693a5d1b
Anamika pandey <anamika.p10902080@gmail.com>
2025-12-28 20:23:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function loginUser(event) {
+    event.preventDefault();
+    alert("Login successful (frontend only)");
+}
+
+function registerUser(event) {
+    event.preventDefault();
+    alert("Registration successful (frontend only)");
+}
